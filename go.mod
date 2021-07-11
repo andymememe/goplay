368556473c290f11d1f7b6fee02021469e0f05d7
Chen, Liang Yu (Andymememe) <andymememe@gmail.com>
2021-07-11 17:48:57 +0800

    Fix: Remove gocui

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/andymememe/goplay
 
 go 1.16
-
-require (
-	github.com/jroimartin/gocui v0.4.0
-	github.com/nsf/termbox-go v1.1.1 // indirect
-)
--- main.go
+++ main.go
@@ -1,29 +1,4 @@
 package main
 
-import (
-	"log"
-
-	"github.com/jroimartin/gocui"
-)
-
 func main() {
-	g, err := gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-		log.Panicln(err)
-	}
-	defer g.Close()
-
-	g.SetManagerFunc(genMainLayout)
-
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
-	}
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
 }
